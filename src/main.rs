0ab0a62ce0eccff6f0bf2e04d4355ad20cdb9362
Meptl <git-client@meptl.com>
2026-05-21 22:58:24 +0200

    Add service foundation dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![forbid(unsafe_code)]
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn executable_package_metadata_is_declared() {
+        assert_eq!(env!("CARGO_PKG_NAME"), "trmnl-whatsapp-list");
+    }
 }
